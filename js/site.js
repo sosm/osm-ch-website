32f0b4631d5ea9d1e40b44af08b97d06b4e92e4e
Sarah Hoffmann <lonvia@denofr.de>
2013-08-11 14:50:20 +0200

    make hash disappear in info view

--- js/site.js
+++ js/site.js
@@ -1,7 +1,8 @@
 var map;
+var maphash = null;
 
 $(document).ready(function() {
-    $('#more-map-selector').html('<a href="#map" class="rotate">Mehr Karte</a>');
+    $('#more-map-selector').html('<a href="#" class="rotate">Mehr Karte</a>');
     $('#map').text('');
     map = L.map('map').setView([47, 8.5], 9);
     L.tileLayer('http://tile.osm.ch/switzerland/{z}/{x}/{y}.png', {
@@ -11,13 +12,21 @@ $(document).ready(function() {
 
     $('.view-selector').click(function() { 
         $("body").toggleClass('map-only'); 
+        if ($("body").hasClass('map-only')) {
+            if (maphash)
+                maphash.startListening();
+            else
+                maphash = new L.Hash(map);
+        } else {
+            maphash.stopListening();
+        }
         map.invalidateSize();
     });
 
     if (/#[0-9]+\//.test(location.hash)) {
         $("body").addClass('map-only');
+        maphash = new L.Hash(map);
         map.invalidateSize();
     }
 
-    hash = new L.Hash(map);
 });
