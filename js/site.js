faccf1122669067e791a2c434b095deaf050e8f1
Sarah Hoffmann <lonvia@denofr.de>
2014-01-20 20:30:42 +0100

    add a 'Fix the map' link

--- js/site.js
+++ js/site.js
@@ -6,14 +6,62 @@ var geosearch = new L.Control.GeoSearch({
                     notFoundMessage: 'Es wurden keine Ergebnisse gefunden'
                 });
 
+
+L.Control.OSMReportAProblem = L.Control.Attribution.extend({
+        options: {
+                position: 'bottomright',
+                prefix: '<a href="http://www.openstreetmap.org/fixthemap?lat={x}&lon={y}&zoom={z}">Behebe einen Fehler</a>'
+        },
+        onAdd: function (map) {
+                this._container = L.DomUtil.create('div', 'leaflet-control-attribution');
+                L.DomEvent.disableClickPropagation(this._container);
+
+                // TODO ugly, refactor
+                for (var i in map._layers) {
+                        if (map._layers[i].getAttribution) {
+                                this.addAttribution(map._layers[i].getAttribution());
+                        }
+                }
+
+                this._update();
+                map.on('moveend', this._update, this);
+
+                return this._container;
+        },
+        _update: function () {
+                if (!this._map) { return; }
+
+                var attribs = [];
+
+                for (var i in this._attributions) {
+                        if (this._attributions[i]) {
+                                attribs.push(i);
+                        }
+                }
+
+                var prefixAndAttribs = [];
+
+                if (this.options.prefix) {
+                        prefixAndAttribs.push(this.options.prefix);
+                }
+                if (attribs.length) {
+                        prefixAndAttribs.push(attribs.join(', '));
+                }
+
+                this._container.innerHTML = prefixAndAttribs.join(' | ').replace('{x}', this._map.getCenter().lat).replace('{y}', this._map.getCenter().lng).replace('{z}', this._map.getZoom());
+        }
+
+        });
+
 $(document).ready(function() {
     $('#more-map-selector').html('<a href="#" class="rotate">Mehr Karte</a>');
     $('#map').text('');
-    map = L.map('map').setView([47, 8.5], 9);
+    map = L.map('map', {attributionControl: false}).setView([47, 8.5], 9);
     L.tileLayer('http://tile.osm.ch/switzerland/{z}/{x}/{y}.png', {
         maxZoom: 21,
         attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    (new L.Control.OSMReportAProblem({})).addTo(map);
 
     $('.view-selector').click(function() { 
         $("body").toggleClass('map-only'); 
